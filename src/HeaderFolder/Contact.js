afd8fbad399d7034ce1ae45d3de3d9d7cd722a0f
SANDHYA24THIRUMOORTHY <tsandhya2404@gmail.com>
2024-07-24 21:48:32 +0530

    i

--- src/HeaderFolder/Contact.js
+++ src/HeaderFolder/Contact.js
@@ -1,7 +1,10 @@
 import React from 'react'
-
+import vedio from '../asset/vedio.mp4';
+import './Contact.css';
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className='ved'>
+      <video src={vedio} autoPlay loop muted/>
+    </div>
   )
 }
