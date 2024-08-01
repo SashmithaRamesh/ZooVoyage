eaf7feb605af8432a0623ab70265f4b630fa8639
SANDHYA24THIRUMOORTHY <tsandhya2404@gmail.com>
2024-08-01 11:47:40 +0530

    i

--- src/ZooTicket/Cart.js
+++ src/ZooTicket/Cart.js
@@ -11,40 +11,43 @@ function Cart() {
 
   return (
     <div className='cart'>
-    <div className="cart-page">
-      <p>YOUR CART</p>
-      {cart.map((item, index) => (
-        <div key={index} className="cart-item">
-          <h2>BOOKING FOR: {new Date(item.date).toDateString()}</h2>
-          <div className="tickets">
-            {item.tickets.map((ticket, i) => (
-              <div key={i} className="ticket-detail">
-                <span>{ticket.name}: {ticket.quantity} x ₹{ticket.price}</span>
-              </div>
-            ))}
+      <div className="cart-page">
+        <p>YOUR CART</p>
+        {cart.map((item, index) => (
+          <div key={index} className="cart-item">
+            <h2>Booking for: {new Date(item.date).toDateString()}</h2>
+            <div className="tickets">
+              {item.tickets.map((ticket, i) => (
+                <div key={i} className="ticket-detail">
+                  <span>{ticket.name}</span>
+                  <span>{ticket.quantity} x {ticket.price}</span>
+                </div>
+              ))}
+            </div>
+            <h2>Add-Ons:</h2>
+            <div className="add-ons">
+              {item.addOns.map((addOn, i) => (
+                <div key={i} className="add-on-detail">
+                  <span>{addOn.name}</span>
+                  <span>{addOn.price}</span>
+                </div>
+              ))}
+            </div>
+            <div className="item-total">
+              <strong>Total: {item.totalAmount}</strong>
+            </div>
           </div>
-          <br></br>
-            <h2>ADD-ONs:</h2>
-          <div className="add-ons">
-            {item.addOns.map((addOn, i) => (
-              <div key={i} className="add-on-detail">
-                <span>{addOn.name}: ₹{addOn.price}</span>
-              </div>
-            ))}
-          </div>
+        ))}
+        <div className="cart-total">
+          <h2>Grand Total: {totalAmount}</h2>
         </div>
-      ))}
-      <div className="cart-total">
-        <h2>Grand Total: ₹{totalAmount}</h2>
+        <div>
+          <button onClick={clearCart} className="clear-cart">Clear Cart</button>
+        </div>
+        <div>
+          <button onClick={() => navigate('/payment')} className="pay-button">Proceed to Payment</button>
+        </div>
       </div>
-      <div>
-        <button onClick={clearCart} className="clear-cart">Clear Cart</button>
-      </div>
-      <br></br>
-      <div>
-        <button onClick={() => navigate('/payment')} className="pay-button">Proceed to Payment</button>
-      </div>
-    </div>
     </div>
   );
 }
